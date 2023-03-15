3b787feec83666cc622362b4c2a7154edc79dbca
Tasreen Mowreen <tmowreen97@gmail.com>
2023-03-14 23:37:57 -0400

    correcting category list page

--- client/src/Categories.js
+++ client/src/Categories.js
@@ -1,38 +1,38 @@
 import React, { useEffect, useState } from "react";
-import {useNavigate} from 'react-router-dom';
+import '../src/styles/card.css';import {useNavigate} from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function Categories({categories}){
-  // const [categories, setCategories]= useState([])
   const navigate = useNavigate()
+  // const [categories, setCategories]= useState([])
   // useEffect(()=> {
   //   fetch("/categories").then(r=>r.json()).then(categories => setCategories(categories))
   // },[])
+  console.log(categories)
 
-  categories.map((category)=> {
-    console.log(category.name)
-  })
+  // categories.map((category)=> {
+  //   console.log(category.name)
+  // })
 
   return(
-    categories.map((category)=> {
-      return (
-        <div>
-            <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={category.img}/>
-            <Card.Body>
-              <Card.Title>{category.name}</Card.Title>
-              {/* <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text> */}
-              <Button variant="primary" onClick={()=> navigate(`/categories/${category.id}`) }>Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </div>
-      )
-    })
+    <div className="grid">
+      {categories && categories.map((category)=> {
+        return(
+          <Card style={{ width: '18rem' }} className='box' onClick={()=> navigate(`/categories/${category.id}`)}>
+          <Card.Img variant="top" src={category.img} />
+          <Card.Body>
+            <Card.Title>{category.name}</Card.Title>
+          </Card.Body>
+        </Card>
+        )
+      })}
+    </div>
   )
 }
 
 export default Categories;
+
+
